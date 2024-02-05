e0e7dbf481ab7ddeff9665f87d6bcc79d117ed53
boazmr <158840378+boazmr@users.noreply.github.com>
2024-02-05 10:21:17 +0200

    RLEListAppend - not woring propperly, uploding for test

--- RLEList.c
+++ RLEList.c
@@ -2,7 +2,7 @@
 #include "RLEList.h"
 
 struct RLEList_t{
-    char value;
+    char val;
     int repetitions;
     struct RLEList_t *next;
 };
@@ -18,10 +18,34 @@ RLEList RLEListCreate(){
 }
 
 void RLEListDestroy(RLEList list){
-    while(list)
-    {
+    while(list){
         RLEList toDelete = list;
         list = list -> next;
         free(toDelete);
     }
+}
+
+
+RLEListResult RLEListAppend(RLEList list, char value){
+    if(!list){ /*list == NULL*/
+        return RLE_LIST_NULL_ARGUMENT;
+    }
+    RLEList nextNode = list -> next;
+    while(nextNode){ /*going to the last node */
+        list = nextNode;
+        nextNode = list -> next;
+    } /* list is the last node */
+    if(list -> val == value){ /*checking if need to create new node*/
+        list -> repetitions ++;
+        return RLE_LIST_SUCCESS;
+    }
+    RLEList newNode = malloc(sizeof(RLEList));
+    if(!newNode){
+        return RLE_LIST_OUT_OF_MEMORY;
+    }
+    newNode -> val = value;
+    newNode -> repetitions = 1;
+    newNode -> next = NULL;
+    list -> next = newNode;
+    return RLE_LIST_SUCCESS;
 }
